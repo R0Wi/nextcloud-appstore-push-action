43f3ea0da4bbe276516fb8e1f7baa148104b55fd
Robin Windey <ro.windey@gmail.com>
2020-10-04 15:17:39 +0200

    Add username pw auth and tests

--- push_appstore.sh
+++ push_appstore.sh
@@ -3,15 +3,31 @@
 # Used environment-variables:
 #   APP_NAME
 #   APPSTORE_TOKEN
+#   APPSTORE_USERNAME
+#   APPSTORE_PASSWORD
 #   NIGHTLY
 #   DOWNLOAD_URL
 #   APP_PRIVATE_KEY_FILE
+
+. functions.sh
 
 echo "Downloading app tarball for signing"
 wget "$DOWNLOAD_URL" -O "${APP_NAME}.tar.gz"
 
 echo "Creating signature for app release"
-sign="`openssl dgst -sha512 -sign ${APP_PRIVATE_KEY_FILE} ${APP_NAME}.tar.gz | openssl base64 -A`"
+sign=$(createsign ${APP_PRIVATE_KEY_FILE} ${APP_NAME}.tar.gz) 
 
 echo "Creating new app release in Nextcloud appstore (nightly=${NIGHTLY})"
-curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
+
+if [ ! -z $APPSTORE_TOKEN ]
+then
+    echo "Using token authentication"
+    curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
+elif [ ! -z $APPSTORE_USERNAME  ] && [ ! -z $APPSTORE_PASSWORD ]
+then
+    echo "Using username password authentication"
+    curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -u "${APPSTORE_USERNAME}:${APPSTORE_PASSWORD}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
+else
+    echo "Authentication cannot be done. Please provide 'appstore_token' or 'appstore_username' and 'appstore_password' input variables."
+    exit 1
+fi
---
+++ test/test.bats
@@ -0,0 +1,9 @@
+#!/usr/bin/env bats
+
+@test "Test sign function" {
+    source ../functions.sh
+    expected="MU8K5DbVAJgCnt/LjUnNKiPCPp9CrWgGyCFn/UOwfveDBuV6hjEOylbSwbuEAG9iERdooaykVh/3MHUAdv3xFR03BEIkCW3XEle/w9Su1+r0FGDQYZjS6dm0fUjYTK3f5/+V1zBYhG+g9pWkCqpf7/4d4BiN0HTRjlL8f8fjh62PycPiCJH5czr2s5n9g5WK3t2roamGonQWGeqBILwkVK4plPIbSit5fMN7KHlc0J18izjT8IkkEIZ1Dm0ejiDGydLF699FCkwCFGX3aHpwQGD604JLBP6Y5ZwqX0gaSo5JpNxyhiGolvKn6zai7tpe8ObpaxmZ/KlCQCtdX9Or+ZJXdWhgYrwRxOE3twwNjY5HI2FLHBTUgmCrvXmJgeGr2q/neMAZcOITqzSHe/sJvpBBz0Qx4EVeP7wetgzCDWcmZsgpiA/AUJtDosVPswI4T4s/IBFpsH93EfbA71+G+58gKjAVHml0ayda15DzFb3UlDOwIwujsHrQ5iVcsG4iNn5JBzk/r81RR6LJRSAIpCSJOSJlxOTQCfFpwWRQrI+3E0CVW0y0Hf5WQibE7fO3al3gVq93W/S0oMH90u8OHilPLI3ZAHjSaqVmoQSm9cPoxW738P17N1o79nQBx4fUXjhXRzy1qD78QYU0JyqO4z804akRUiIe/XVylHGIF5w="
+    run createsign test_appkey.key test_app_artifact.tar.gz
+    [ "$status" = 0 ]
+    [ "$output" = $expected ]
+}
