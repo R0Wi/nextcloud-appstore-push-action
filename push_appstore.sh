ef73729a05912f63459a27213b673e25c7099d54
Robin Windey <ro.windey@gmail.com>
2020-10-04 22:47:57 +0200

    Curl fail on non 200

--- push_appstore.sh
+++ push_appstore.sh
@@ -22,11 +22,11 @@ echo "Creating new app release in Nextcloud appstore (nightly=${NIGHTLY})"
 if [ ! -z $APPSTORE_TOKEN ]
 then
     echo "Using token authentication"
-    curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
+    curl -s --fail --show-error -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
 elif [ ! -z $APPSTORE_USERNAME  ] && [ ! -z $APPSTORE_PASSWORD ]
 then
     echo "Using username password authentication"
-    curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -u "${APPSTORE_USERNAME}:${APPSTORE_PASSWORD}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
+    curl -s --fail --show-error -X POST https://apps.nextcloud.com/api/v1/apps/releases -u "${APPSTORE_USERNAME}:${APPSTORE_PASSWORD}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
 else
     echo "Authentication cannot be done. Please provide 'appstore_token' or 'appstore_username' and 'appstore_password' input variables."
     exit 1
