fcaa0b2698be1a8a5b2f5caa84bb3f7df3f14937
Robin Windey <ro.windey@gmail.com>
2020-10-06 06:48:48 +0200

    Try fix function not found error

--- push_appstore.sh
+++ push_appstore.sh
@@ -10,24 +10,27 @@ set -euxo pipefail
 #   DOWNLOAD_URL
 #   APP_PRIVATE_KEY_FILE
 
-. functions.sh
+script_dir="$(dirname "$0")"
+appstore_url="https://apps.nextcloud.com/api/v1/apps/releases"
+
+"${script_dir}/functions.sh"
 
 echo "Downloading app tarball for signing"
 wget "$DOWNLOAD_URL" -O "${APP_NAME}.tar.gz"
 
 echo "Creating signature for app release"
-sign=$(createsign ${APP_PRIVATE_KEY_FILE} ${APP_NAME}.tar.gz) 
+sign="`createsign ${APP_PRIVATE_KEY_FILE} ${APP_NAME}.tar.gz`"
 
 echo "Creating new app release in Nextcloud appstore (nightly=${NIGHTLY})"
 
 if [ ! -z $APPSTORE_TOKEN ]
 then
     echo "Using token authentication"
-    curl -s --fail --show-error -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
+    curl -s --fail --show-error -X POST $appstore_url -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
 elif [ ! -z $APPSTORE_USERNAME  ] && [ ! -z $APPSTORE_PASSWORD ]
 then
     echo "Using username password authentication"
-    curl -s --fail --show-error -X POST https://apps.nextcloud.com/api/v1/apps/releases -u "${APPSTORE_USERNAME}:${APPSTORE_PASSWORD}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
+    curl -s --fail --show-error -X POST $appstore_url -u "${APPSTORE_USERNAME}:${APPSTORE_PASSWORD}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
 else
     echo "Authentication cannot be done. Please provide 'appstore_token' or 'appstore_username' and 'appstore_password' input variables."
     exit 1
